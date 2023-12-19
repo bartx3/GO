4768f609898ba2c314370449613fa8cbf26d2677
nat <nataliakopik1@gmail.com>
2023-12-20 00:05:51 +0100

    FXML added?

---
+++ client/src/main/java/go/client/BoardFacade.java
@@ -0,0 +1,5 @@
+package go.client;
+
+public interface BoardFacade {
+
+}
---
+++ client/src/main/java/go/client/Move.java
@@ -0,0 +1,9 @@
+package go.client;
+
+public class Move {
+    public Move(int x, int y) {
+        /**/
+        System.out.println("Give the x coordinate[1-19]: ");
+
+    }
+}
