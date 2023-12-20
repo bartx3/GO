01b5813d7aa1ea688c32b9016f493b6b155ed758
nat <nataliakopik1@gmail.com>
2023-12-20 02:48:43 +0100

    Moves are created

--- client/src/main/java/go/client/Main.java
+++ client/src/main/java/go/client/Main.java
@@ -9,16 +9,40 @@ public class Main {
             System.out.println("player black "+i+"th move");
             System.out.println("Give the x coordinate[1-19]: ");
             int black_x = scan.nextInt();
+
             System.out.println("Give the y coordinate[1-19]: ");
             int black_y = scan.nextInt();
-            new MoveCreater(black_x,black_y);
+            MoveCreater black_move=new MoveCreater(black_x,black_y);
+
+            if(black_move.checkMove()==true){
+                System.out.println("wrong coordinates, try again");
+                System.out.println("player black "+i+"th move");
+                System.out.println("Give the x coordinate[1-19]: ");
+                black_x = scan.nextInt();
+
+                System.out.println("Give the y coordinate[1-19]: ");
+                black_y = scan.nextInt();
+                black_move=new MoveCreater(black_x,black_y);
+            }
 
             System.out.println("player white "+i+"th move");
             System.out.println("Give the x coordinate[1-19]: ");
             int white_x = scan.nextInt();
+
             System.out.println("Give the y coordinate[1-19]: ");
             int white_y = scan.nextInt();
-            new MoveCreater(white_x,white_y);
+            MoveCreater white_move=new MoveCreater(white_x,white_y);
+
+            if(white_move.checkMove()==true){
+                System.out.println("wrong coordinates, try again");
+                System.out.println("player white "+i+"th move");
+                System.out.println("Give the x coordinate[1-19]: ");
+                white_x = scan.nextInt();
+
+                System.out.println("Give the y coordinate[1-19]: ");
+                white_y = scan.nextInt();
+                white_move=new MoveCreater(white_x,white_y);
+            }
 
         }
     }
--- client/src/main/java/go/client/MoveChecker.java
+++ client/src/main/java/go/client/MoveChecker.java
@@ -2,6 +2,8 @@ package go.client;
 
 public class MoveChecker implements Gamestate{
     Move move;
+    //do storowania koordynatów przyda sie tez jakis playerhandler, kótry później sprawdzi z ztymi oddechami
+    int coordinates_list[][]=new int[360][360];
 
     @Override
     public Move createMove() {
@@ -10,13 +12,27 @@ public class MoveChecker implements Gamestate{
 
     @Override
     public boolean checkMove() {
+        boolean flag=false;
         int checked_x=move.getX();
         int checked_y=move.getY();
-        return false;
+
+        int checked[][]=new int[1][1];
+        int i1 = checked[checked_x][checked_y];
+
+        //czy ten ruch nie był już zrobiony
+        for(int i=0;i<=360;i++){
+            for (int j=0;j<=360;j++) {
+                if (i1==coordinates_list[i][j]) {
+                    flag=true;
+                }
+            }
+        }
+
+            //czy ten ruch zabierze ostatni oddech tego gracza?
+        return flag;
     }
 
     @Override
     public void makeMove() {
-
     }
 }
