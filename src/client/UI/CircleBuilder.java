6798070880577ca31011974cc92e232176d62668
kopiautopia <136523965+kopiautopia@users.noreply.github.com>
2024-01-09 18:36:15 +0100

    Add files via upload

---
+++ src/client/UI/CircleBuilder.java
@@ -0,0 +1,36 @@
+package client.UI;
+
+import java.util.ArrayList;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+public class CircleBuilder {
+    public ArrayList<Circle> circles = new ArrayList<>();
+    double x,y;
+    public CircleBuilder(SquareBuilder square){
+        x= square.getX_coordinate();
+        setCircleX(x);
+        y= square.getY_coordinate();
+        setCircleX(y);
+
+        Circle circle=new Circle(x,y,25);
+        rectangle.setFill(Color.TRANSPARENT);
+        rectangle.setStroke(Color.TRANSPARENT);
+        circles.add(circle);
+    }
+
+    public void setCircleX(double x) {
+        this.x = x;
+    }
+
+    public void setCircleY(double y) {
+        this.y=y;
+    }
+
+    public double getCircleX() {
+        return x;
+    }
+
+    public double getCircleY() {
+        return y;
+    }
+}
---
+++ src/client/UI/SquareBuilder.java
@@ -0,0 +1,33 @@
+package client.UI;
+
+import javafx.scene.shape.Rectangle;
+import javafx.scene.paint.Color;
+
+
+public class SquareBuilder {
+    int x_coordinate;
+    int y_coordinate;
+    public SquareBuilder(int i, int n){
+        x_coordinate=10+50*(i%n);
+        y_coordinate= (int) (10+Math.floor(i/n));
+        setX_coordinate(x_coordinate);
+        setY_coordinate(y_coordinate);
+        Rectangle square=new Rectangle(x_coordinate, y_coordinate, 50, 50);
+        rectangle.setFill(Color.TRANSPARENT);
+        rectangle.setStroke(Color.BLACK);
+    }
+    public void setX_coordinate(int x_coordinate){
+        this.x_coordinate=x_coordinate;
+    }
+    public void setY_coordinate(int y_coordinate){
+        this.y_coordinate=y_coordinate;
+    }
+
+    public int getX_coordinate() {
+        return x_coordinate;
+    }
+
+    public int getY_coordinate() {
+        return y_coordinate;
+    }
+}
